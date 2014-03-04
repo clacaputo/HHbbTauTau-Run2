f465596568b807856262d2eac07698dbf01d0a6c
Konstantin <konstantin.androsov@gmail.com>
2014-03-04 17:26:39 +0100

    Fixed patTuple.py to run with TauID 2014 code.

--- PatProduction/python/patTuple.py
+++ PatProduction/python/patTuple.py
@@ -1,43 +1,5 @@
-import FWCore.ParameterSet.Config as cms
-
-process = cms.Process("PAT")
-
-## MessageLogger
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
-
-## Options and Output Report
-process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-
-## Source
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring()
-)
-## Maximal Number of Events
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
-
-## Geometry and Detector Conditions (needed for a few patTuple production steps)
-process.load("Configuration.Geometry.GeometryIdeal_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-#from Configuration.AlCa.autoCond import autoCond
-#process.GlobalTag.globaltag = cms.string( autoCond[ 'startup' ] )
-process.GlobalTag.globaltag = cms.string( 'START53_V27::All' )
-process.load("Configuration.StandardSequences.MagneticField_cff")
-
-## Output Module Configuration (expects a path 'p')
-from PhysicsTools.PatAlgos.patEventContent_cff import patEventContent
-process.out = cms.OutputModule("PoolOutputModule",
-                               fileName = cms.untracked.string('patTuple.root'),
-                               # save only events passing the full path
-                               SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
-                               # save PAT Layer 1 output; you need a '*' to
-                               # unpack the list of commands 'patEventContent'
-                               outputCommands = cms.untracked.vstring('drop *', *patEventContent )
-                               )
-
-process.outpath = cms.EndPath(process.out)
-
 ## Import skeleton process.
-#from PhysicsTools.PatAlgos.patTemplate_cfg import *
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
 process.options.wantSummary = False
 
 ## Parse and apply options.
@@ -67,9 +29,8 @@ process.out.fileName = options.outputFile
 ## Include high Pt tau reconstruction sequence.
 process.load("RecoTauTag.Configuration.RecoPFTauTag_cff")
 
-## Standard PAT Configuration File
-process.load("PhysicsTools.PatAlgos.patSequences_cff")
-
+from PhysicsTools.PatAlgos.tools.tauTools import *
+switchToPFTauHPS(process)
 
 ## Define process path.
 process.p = cms.Path( process.PFTau * process.patDefaultSequence )
