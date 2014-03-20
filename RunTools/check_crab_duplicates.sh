22970efa9086013740ea2ce91df1cfc1a6ae4b2a
Konstantin <konstantin.androsov@gmail.com>
2014-03-20 19:32:30 +0100

    Improved RunTools/check_crab_duplicates.sh script.

--- RunTools/check_crab_duplicates.sh
+++ RunTools/check_crab_duplicates.sh
@@ -9,7 +9,15 @@ fi
 DIR_NAME=$1
 PREFIX=$2
 
-ls $DIR_NAME | \
-    sed "s/\(${PREFIX}_\)\([0-9]*\)\(.*\)/\2/" | sort | uniq -d | \
-    sed "s/\(.*\)/${PREFIX}_\1_\*/" | \
-    xargs -n 1 find $DIR_NAME -name
+TOTAL_NUMBER_OF_FILES=$( ls $DIR_NAME | wc -l )
+
+echo "Total number of files in the directory: $TOTAL_NUMBER_OF_FILES"
+
+FILE_LIST=$( ls $DIR_NAME | sed "s/\(${PREFIX}_\)\([0-9]*\)\(.*\)/\2/" | sort | uniq -d )
+
+if [ "x$FILE_LIST" = "x" ] ; then
+	echo "There are no jobs that have more than one output file."
+else
+	echo "IDs of the jobs that have more than one output file:" $FILE_LIST
+	echo "$FILE_LIST" | sed "s/\(.*\)/${PREFIX}_\1_\*/" | xargs -n 1 find $DIR_NAME -name | xargs ls -al
+fi
