824fe46af47b7ff221ace7e977da2e74e6e7c1d1
Konstantin <konstantin.androsov@gmail.com>
2014-03-20 19:55:32 +0100

    Modified generate_file_list.sh.
    - RunTools/generate_file_list.sh: Now, as a third argument, script takes number of jobs instead on number of files per job.

--- RunTools/generate_file_list.sh
+++ RunTools/generate_file_list.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -le 2 -o $# -ge 5 ] ; then
-    echo "Usage: input_path output_file_name n_split [reference_path]"
+    echo "Usage: input_path output_file_name n_jobs [reference_path]"
     exit
 fi
 
@@ -11,7 +11,7 @@ OUT_FILE_NAME=$(basename $2)
 OUT_FILE=$OUT_DIR/$OUT_FILE_NAME
 TMP_DIR=$(mktemp -d)
 TMP_OUT_FILE=$TMP_DIR/$OUT_FILE_NAME
-N_SPLIT=$3
+N_JOBS=$3
 REFERENCE_PATH=$4
 PREFIX=""
 
@@ -25,6 +25,11 @@ fi
 
 find  $INPUT_PATH -name "*.root" -printf "${PREFIX}%p\n" > $TMP_OUT_FILE
 cd $TMP_DIR
+N_FILES=$( cat $TMP_OUT_FILE | wc -l )
+N_SPLIT=$(( (N_FILES + N_JOBS - 1) / N_JOBS ))
+echo "Total number of files: $N_FILES"
+echo "Total number of jobs: $N_JOBS"
+echo "Number of files per job: $N_SPLIT"
 split -l $N_SPLIT $TMP_OUT_FILE
 find . -maxdepth 1 -type f -name "x*" -printf "%f ${OUT_FILE_NAME}_%f.txt\n" | xargs -n 2 mv
 mv *.txt $OUT_DIR
