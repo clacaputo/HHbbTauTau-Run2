933ccfc3c6519a52c97fbc906e96d108d75c1ade
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-03-21 18:16:08 +0100

    Modified batch script for Pisa to get parameters from outside.

---
+++ RunTools/runTreeProducer_BatchPisa.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ne 7 ] ; then
+    echo "Usage: job_name working_path file_list_path output_path global_tag include_sim n_events"
+    exit
+fi
+
+NAME=$1
+WORKING_PATH=$( echo $(cd $2; pwd) )
+FILE_LIST_PATH=$3
+OUTPUT_PATH=$4
+GLOBAL_TAG=$5
+INCLUDE_SIM=$6
+N_EVENTS=$7
+
+cd $WORKING_PATH
+source cmsenv.sh
+eval $( scramv1 runtime -sh )
+
+cmsRun TreeProduction/python/treeProducer.py globalTag=$GLOBAL_TAG includeSim=$INCLUDE_SIM \
+       fileList=$FILE_LIST_PATH/${NAME}.txt maxEvents=$N_EVENTS outputFile=$OUTPUT_PATH/${NAME}_Tree.root \
+       > $OUTPUT_PATH/${NAME}_detail.log 2> $OUTPUT_PATH/${NAME}.log
