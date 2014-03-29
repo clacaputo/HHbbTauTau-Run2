58a677b8067ddfe557a1252708cbc3b648a96c32
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-03-29 12:54:37 +0100

    Created DebugTool.h and removed EventTree.h.
    - Analysis/include/DebugTools.h: file to put common tools and
      definitions suitable for debug purposes.
    	- Implemented function PrintCommonTypeSizes to print size in bytes
    	  of common simple types used in the code.

---
+++ Analysis/include/DebugTools.h
@@ -0,0 +1,37 @@
+/*!
+ * \file DebugTools.h
+ * \brief Common tools and definitions suitable for debug purposes.
+ * \author Konstantin Androsov (INFN Pisa, Siena University)
+ * \author Maria Teresa Grippo (INFN Pisa, Siena University)
+ * \date 2014-03-29 created
+ */
+
+#pragma once
+
+#include<vector>
+
+namespace debug {
+
+#define PRINT_SIZEOF(name) \
+    std::cout << "Sizeof " #name " = " << sizeof(name) << std::endl
+
+inline void PrintCommonTypeSizes()
+{
+    PRINT_SIZEOF(Float_t);
+    PRINT_SIZEOF(Double_t);
+    PRINT_SIZEOF(Int_t);
+    PRINT_SIZEOF(UInt_t);
+    PRINT_SIZEOF(Bool_t);
+    PRINT_SIZEOF(int);
+    PRINT_SIZEOF(unsigned);
+    PRINT_SIZEOF(float);
+    PRINT_SIZEOF(double);
+    PRINT_SIZEOF(bool);
+    PRINT_SIZEOF(size_t);
+
+    typedef std::vector<double>::size_type std_collection_size_type;
+    PRINT_SIZEOF(std_collection_size_type);
+}
+#undef PRINT_SIZEOF
+
+} // debug
