6c4b57f1444bde0cab9c6b84f76b266fce3b0cda
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-03-30 14:51:20 +0200

    Fixed env.sh to be compatible with gridui setup.

--- env.sh
+++ env.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cmsenv
-export GCC_INCLUDE_PATH=$( echo $CMSSW_RELEASE_BASE/../../../external/gcc/*/include/c++/* )
-export BOOST_INCLUDE_PATH=$( echo $CMSSW_RELEASE_BASE/../../../external/boost/*/include )
-export ROOT_INCLUDE_PATH=$( echo $CMSSW_RELEASE_BASE/../../../lcg/root/*/include )
-export HEPMC_INCLUDE_PATH=$( echo $CMSSW_RELEASE_BASE/../../../external/hepmc/*/include )
+export CMSSW_ROOT_PATH=$( cd $CMSSW_DATA_PATH/.. ; pwd )
+
+export GCC_PATH=$( find $CMSSW_ROOT_PATH/external/gcc/ -maxdepth 1 -type d | tail -n 1 )
+export GCC_INCLUDE_PATH=$( find $GCC_PATH/include/c++/ -maxdepth 1 -type d | tail -n 1 )
+
+export BOOST_INCLUDE_PATH=$( find $CMSSW_ROOT_PATH/external/boost/ -maxdepth 1 -type d | tail -n 1 )/include
+export ROOT_INCLUDE_PATH=$( find $CMSSW_ROOT_PATH/lcg/root/ -maxdepth 1 -type d | tail -n 1 )/include
+export HEPMC_INCLUDE_PATH=$( find $CMSSW_ROOT_PATH/external/hepmc/ -maxdepth 1 -type d | tail -n 1 )/include
