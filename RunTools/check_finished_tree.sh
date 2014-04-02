348d1830e84002885907619853d54dd83d7427e5
Maria Teresa <grippomariateresa@gmail.com>
2014-04-02 18:34:12 +0200

    Implemented RunTools/check_finished_tree.sh to check finished tree

--- RunTools/check_finished_tree.sh
+++ RunTools/check_finished_tree.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-if [ $# -ne 1 ] ; then
-    echo "Usage: file_list_path "
+if [ $# -ne 3 ] ; then
+    echo "Usage: file_list_path job_result new_file_list_path"
     exit
 fi
 
 FILE_LIST_PATH=$1
+FILE_JOB_RESULT=$2
+NEW_FILE_LIST_PATH=$3
 
 
-WORKING_PATH=$PWD
-
-if [ ! -d "$WORKING_PATH" ] ; then
-        echo "ERROR: working path '$WORKING_PATH' does not exist."
-        exit
-fi
-
-if [ ! -d "$WORKING_PATH/$FILE_LIST_PATH" ] ; then
-        echo "ERROR: file list path '$WORKING_PATH/$FILE_LIST_PATH' does not exist."
+if [ ! -d "$FILE_LIST_PATH" ] ; then
+        echo "ERROR: file list path '$FILE_LIST_PATH' does not exist."
         exit
 fi
 
 
-JOBS=$( find $WORKING_PATH/$FILE_LIST_PATH -maxdepth 1 -name "*.txt" -printf "%f\n" )
+JOBS=$( find $FILE_LIST_PATH -maxdepth 1 -name "*.txt" -printf "%f\n" | sed "s/\.txt//" | sort )
 
 if [ "x$JOBS" = "x" ] ; then
         echo "ERROR: directory '$FILE_LIST_PATH' does not contains any job description."
         exit
 fi
 
-N_JOBS=$( echo "$JOBS" | wc -l )
-echo "Following jobs will be submited:" $JOBS
-echo "Total number of jobs to submit: $N_JOBS"
+#N_JOBS=$( echo "$JOBS" | wc -l )
+#echo "Following jobs will be submited:" $JOBS
+#echo "Total number of jobs to submit: $N_JOBS"
+
+SUCCESSFULL_JOBS=$( cat $FILE_JOB_RESULT | sed -n "s/\(^0 \)\([^ ]*\)\(.*\)/\2/p" | sort )
+
+echo -e "$JOBS"\\n"$SUCCESSFULL_JOBS" | sort | uniq -u | xargs -n 1 printf "$FILE_LIST_PATH/%b.txt $NEW_FILE_LIST_PATH/\n" \
+     | xargs -n 2 cp 
+
+
+
+
+
