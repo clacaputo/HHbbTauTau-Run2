36389f0cba696fe9bd32638a96238d90d7d8a22a
Maria Teresa <grippomariateresa@gmail.com>
2014-04-09 15:36:59 +0200

    MOdified RunTools/hadd_analysis.sh - to be finished

--- RunTools/hadd_analysis.sh
+++ RunTools/hadd_analysis.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-if [ $# -ne 2 ] ; then
-    echo "Usage: analysis_path output_path"
+if [ $# -ne 1 ] ; then
+    echo "Usage: analysis_path"
     exit
 fi
 
 ANALYSIS_PATH=$1
+cd $ANALYSIS_PATH
+
+FOLDER_PATH="anaMuTau" "anaETau" "anaTauTau"
+OUTPUT_DIR="anaResult"
+
+mkdir -p $OUTPUT_DIR
+
+for FOLDER in $FOLDER_PATH ; do
+    SUB_FOLDER=$( find $FOLDER -maxdepth 1 -type d -printf "%f\n" )
+    echo $SUB_FOLDER
+done
