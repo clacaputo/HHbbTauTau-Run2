f83e25b961dc5eb3b37328d6f0f4f8285b82cc84
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-04-30 11:46:19 +0200

    Added SkimFilter for PAT production.
    - PatProduction/plugins/SkimFilter.cc: copied Rosma's code with some
      adjustments.

---
+++ PatProduction/plugins/SkimFilter.cc
@@ -0,0 +1,68 @@
+/*!
+ * \file SkimFilter.cc
+ * \brief Definition of SkimFilter class which applies skim for X->HH->bbTauTau PAT event selection.
+ * \author Rosamaria Venditti (INFN Bari, Bari University)
+ * \date 2014-04-30 created
+ */
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDFilter.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Tau.h"
+
+class SkimFilter : public edm::EDFilter {
+public:
+    explicit SkimFilter(const edm::ParameterSet& iConfig)
+        : muonTag(iConfig.getUntrackedParameter<edm::InputTag>("muonSrc")),
+          electronTag(iConfig.getUntrackedParameter<edm::InputTag>("electronSrc")),
+          tauTag(iConfig.getUntrackedParameter<edm::InputTag>("tauSrc")) {}
+
+private:
+    virtual bool filter(edm::Event&, const edm::EventSetup&);
+
+private:
+    edm::InputTag muonTag;
+    edm::InputTag electronTag;
+    edm::InputTag tauTag;
+};
+
+bool SkimFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+    edm::Handle<pat::MuonCollection> hMuons;
+    iEvent.getByLabel(muonTag, hMuons);
+    const pat::MuonCollection& muons = *hMuons.product();
+
+    edm::Handle<pat::ElectronCollection> hElectrons;
+    iEvent.getByLabel(electronTag, hElectrons);
+    const pat::ElectronCollection& electrons = *hElectrons.product();
+
+    edm::Handle<pat::TauCollection> hTaus;
+    iEvent.getByLabel(tauTag, hTaus);
+    const pat::TauCollection& taus = *hTaus.product();
+
+    unsigned nGoodTaus = 0;
+
+    for(const pat::Tau& tau : taus) {
+        if(tau.pt() > 15.0 && std::abs(tau.eta()) < 2.3  && tau.decayMode() > 0.5)
+            ++nGoodTaus;
+        if(nGoodTaus >= 2) return true;
+    }
+    if(!nGoodTaus) return false;
+
+    for(const pat::Electron& electron : electrons) {
+        if(electron.pt() > 15.0 && std::abs(electron->eta()) < 2.5) return true;
+    }
+
+    for(const pat::Muon& muon : muons) {
+        if(muon.pt() > 15.0 && std::abs(muon.eta())<2.4 && muon.isGlobalMuon()) return true;
+    }
+
+    return false;
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(SkimFilter);
