b6555e14ff730e816e063dcb5ce159ffdff6649e
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-05-02 11:56:20 +0200

    Added post-recoil correction code.
    - Analysis/include/BaseAnalyzer.h: included PostRecoilCorrection.h.
    - Analysis/include/PostRecoilCorrection.h: defined header where wrapper
      for RecoilCorrector and include code from RecoilCorrector_v7 package.
    - RecoilCorrector_v7/RecoilCorrector.hh: removed global using.
    - SVfit_standalone/src/SVfitStandaloneMarkovChainIntegrator.cc: moved
      anonymous enums in svFitStandalone namespace to not conflict with
      constants defined in ROOT headers.

---
+++ Analysis/include/PostRecoilCorrection.h
@@ -0,0 +1,16 @@
+/*!
+ * \file PostRecoilCorrection.h
+ * \brief Definition of wrapper for RecoilCorrector code to apply post-recoil correction.
+ * \author Konstantin Androsov (INFN Pisa, Siena University)
+ * \author Maria Teresa Grippo (INFN Pisa, Siena University)
+ * \date 2014-05-02 created
+ */
+
+#pragma once
+
+#include "HHbbTauTau/RecoilCorrector_v7/RecoilCorrector.hh"
+
+namespace analysis {
+
+
+}
