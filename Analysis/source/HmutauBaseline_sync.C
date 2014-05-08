3dc59eb493f84c0dc0aad4d175bb5e97e1de0904
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-05-08 11:36:16 +0200

    Fixed SelectBackgroundElectron in HmutauBaseline_sync.C.

--- Analysis/source/HmutauBaseline_sync.C
+++ Analysis/source/HmutauBaseline_sync.C
@@ -127,21 +127,22 @@ protected:
         return analysis::Candidate(analysis::Candidate::Tau, id, object,object.charge);
     }
 
-    virtual analysis::Candidate SelectBackgroundElectron(size_t id, cuts::ObjectSelector& objectSelector, bool enabled,
-                                                         root_ext::AnalyzerData& _anaData)
+    virtual analysis::Candidate SelectBackgroundElectron(size_t id, cuts::ObjectSelector* objectSelector,
+                                                         root_ext::AnalyzerData& _anaData,
+                                                         const std::string& selection_label)
     {
-        using namespace cuts::Htautau_Summer13::electronID::veto;
-        const std::string selection_label = "electron_bkg";
-        cuts::Cutter cut(objectSelector, enabled);
+        using namespace cuts::Htautau_Summer13::MuTau::electronVeto;
+        cuts::Cutter cut(objectSelector);
         const ntuple::Electron& object = event.electrons().at(id);
 
         cut(true, ">0 ele cand");
         cut(X(pt, 1000, 0.0, 1000.0) > pt, "pt");
-        const double eta = std::abs( X(eta, 120, -6.0, 6.0) );
-        cut(eta < eta_high && (eta < cuts::Htautau_Summer13::electronID::eta_CrackVeto_low ||
-                               eta > cuts::Htautau_Summer13::electronID::eta_CrackVeto_high), "eta");
+        cut(std::abs( X(eta, 120, -6.0, 6.0) ) < eta_high, "eta");
         const double DeltaZ = std::abs(object.vz - primaryVertex.position.Z());
         cut(Y(DeltaZ, 6000, 0.0, 60.0)  < dz, "dz");
+        const TVector3 ele_vertex(object.vx, object.vy, object.vz);
+        const double d0_PV = (ele_vertex - primaryVertex.position).Perp(); // same as dB
+        cut(std::abs( Y(d0_PV, 50, 0.0, 0.5) ) < d0, "d0");
         cut(X(pfRelIso, 1000, 0.0, 100.0) < pFRelIso, "pFRelIso");
         const size_t pt_index = object.pt < ref_pt ? 0 : 1;
         const size_t eta_index = eta < scEta_min[0] ? 0 : (eta < scEta_min[1] ? 1 : 2);
