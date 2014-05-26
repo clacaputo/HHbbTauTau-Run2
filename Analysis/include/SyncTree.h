7f9dc267624ddf716d0ec5d3ce66236c9a295f35
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-05-26 11:12:36 +0200

    Modified SyncTree definition according MSSM H->hh.

--- Analysis/include/SyncTree.h
+++ Analysis/include/SyncTree.h
@@ -11,149 +11,132 @@
 #include "HHbbTauTau/TreeProduction/interface/SmartTree.h"
 
 #define SYNC_DATA() \
-    SIMPLE_VAR(Int_t, NUP) \
-    SIMPLE_VAR(Int_t, run) \
-    SIMPLE_VAR(Int_t, lumi) \
-    SIMPLE_VAR(Int_t, evt) \
-    SIMPLE_VAR(Int_t, npv) \
-    SIMPLE_VAR(Int_t, npu) \
-    SIMPLE_VAR(Double_t, rho) \
-    SIMPLE_VAR(Double_t, mcweight) \
-    SIMPLE_VAR(Double_t, puweight) \
-    SIMPLE_VAR(Double_t, effweight) \
-    SIMPLE_VAR(Double_t, decaymodeweight) \
-    SIMPLE_VAR(Double_t, weight) \
-    SIMPLE_VAR(Double_t, m_sv) \
-    SIMPLE_VAR(Double_t, pt_sv) \
-    SIMPLE_VAR(Double_t, eta_sv) \
-    SIMPLE_VAR(Double_t, phi_sv) \
-    SIMPLE_VAR(Double_t, mvis) \
-    SIMPLE_VAR(Double_t, m_sv_Up) \
-    SIMPLE_VAR(Double_t, m_sv_Down) \
-    /*1st tau*/ \
-    SIMPLE_VAR(Double_t, gen_pt_1) \
-    SIMPLE_VAR(Double_t, pt_1) \
-    SIMPLE_VAR(Double_t, phi_1) \
-    SIMPLE_VAR(Double_t, eta_1) \
-    SIMPLE_VAR(Double_t, m_1) \
-    SIMPLE_VAR(Double_t, q_1) \
-    SIMPLE_VAR(Double_t, iso_1) \
-    SIMPLE_VAR(Double_t, antiele_1) \
-    SIMPLE_VAR(Double_t, d0_1) \
-    SIMPLE_VAR(Double_t, dZ_1) \
-    SIMPLE_VAR(Int_t, passid_1) \
-    SIMPLE_VAR(Int_t, passiso_1) \
-    SIMPLE_VAR(Double_t, mt_1) \
-    SIMPLE_VAR(Double_t, trigweight_1) \
-    SIMPLE_VAR(Double_t, idweight_1) \
-    SIMPLE_VAR(Double_t, isoweight_1) \
-    SIMPLE_VAR(Double_t, byCombinedIsolationDeltaBetaCorrRaw3Hits_1) \
-    SIMPLE_VAR(Double_t, againstElectronMVA3raw_1) \
-    SIMPLE_VAR(Double_t, againstElectronMVA3category_1) \
-    SIMPLE_VAR(Double_t, byIsolationMVA2raw_1) \
-    SIMPLE_VAR(Double_t, againstMuonLoose_1) \
-    SIMPLE_VAR(Double_t, againstMuonLoose2_1) \
-    SIMPLE_VAR(Double_t, againstMuonMedium2_1) \
-    SIMPLE_VAR(Double_t, againstMuonTight2_1) \
-    SIMPLE_VAR(Double_t, againstElectronLooseMVA3_1) \
-    SIMPLE_VAR(Double_t, againstElectronLoose_1) \
-    SIMPLE_VAR(Double_t, againstElectronNewLooseMVA3_1) \
-    /*2nd tau*/ \
-    SIMPLE_VAR(Double_t, gen_pt_2) \
-    SIMPLE_VAR(Double_t, pt_2) \
-    SIMPLE_VAR(Double_t, phi_2) \
-    SIMPLE_VAR(Double_t, eta_2) \
-    SIMPLE_VAR(Double_t, m_2) \
-    SIMPLE_VAR(Double_t, q_2) \
-    SIMPLE_VAR(Double_t, iso_2) \
-    SIMPLE_VAR(Double_t, antiele_2) \
-    SIMPLE_VAR(Double_t, d0_2) \
-    SIMPLE_VAR(Double_t, dZ_2) \
-    SIMPLE_VAR(Int_t, passid_2) \
-    SIMPLE_VAR(Int_t, passiso_2) \
-    SIMPLE_VAR(Double_t, mt_2) \
-    SIMPLE_VAR(Double_t, trigweight_2) \
-    SIMPLE_VAR(Double_t, idweight_2) \
-    SIMPLE_VAR(Double_t, isoweight_2) \
-    SIMPLE_VAR(Double_t, byCombinedIsolationDeltaBetaCorrRaw3Hits_2) \
-    SIMPLE_VAR(Double_t, againstElectronMVA3raw_2) \
-    SIMPLE_VAR(Double_t, againstElectronMVA3category_2) \
-    SIMPLE_VAR(Double_t, byIsolationMVA2raw_2) \
-    SIMPLE_VAR(Double_t, againstMuonLoose_2) \
-    SIMPLE_VAR(Double_t, againstMuonLoose2_2) \
-    SIMPLE_VAR(Double_t, againstMuonMedium2_2) \
-    SIMPLE_VAR(Double_t, againstMuonTight2_2) \
-    SIMPLE_VAR(Double_t, againstElectronLooseMVA3_2) \
-    SIMPLE_VAR(Double_t, againstElectronLoose_2) \
-    SIMPLE_VAR(Double_t, againstElectronNewLooseMVA3_2) \
-    /*met*/ \
-    SIMPLE_VAR(Double_t, met) \
-    SIMPLE_VAR(Double_t, metphi) \
-    SIMPLE_VAR(Double_t, mvamet) \
-    SIMPLE_VAR(Double_t, mvametphi) \
-    SIMPLE_VAR(Double_t, pzetavis) \
-    SIMPLE_VAR(Double_t, pzetamiss) \
-    SIMPLE_VAR(Double_t, metcov00) \
-    SIMPLE_VAR(Double_t, metcov01) \
-    SIMPLE_VAR(Double_t, metcov10) \
-    SIMPLE_VAR(Double_t, metcov11) \
-    SIMPLE_VAR(Double_t, mvacov00) \
-    SIMPLE_VAR(Double_t, mvacov01) \
-    SIMPLE_VAR(Double_t, mvacov10) \
-    SIMPLE_VAR(Double_t, mvacov11) \
-    /*jet*/ \
-    SIMPLE_VAR(Double_t, jpt_1) \
-    SIMPLE_VAR(Double_t, jeta_1) \
-    SIMPLE_VAR(Double_t, jphi_1) \
-    SIMPLE_VAR(Double_t, jptraw_1) \
-    SIMPLE_VAR(Double_t, jptunc_1) \
-    SIMPLE_VAR(Double_t, jmva_1) \
-    SIMPLE_VAR(Int_t, jpass_1) \
-    SIMPLE_VAR(Double_t, jpt_2) \
-    SIMPLE_VAR(Double_t, jeta_2) \
-    SIMPLE_VAR(Double_t, jphi_2) \
-    SIMPLE_VAR(Double_t, jptraw_2) \
-    SIMPLE_VAR(Double_t, jptunc_2) \
-    SIMPLE_VAR(Double_t, jmva_2) \
-    SIMPLE_VAR(Int_t, jpass_2) \
-    SIMPLE_VAR(Double_t, bpt) \
-    SIMPLE_VAR(Double_t, beta) \
-    SIMPLE_VAR(Double_t, bphi) \
-    SIMPLE_VAR(Double_t, mjj) \
-    SIMPLE_VAR(Double_t, jdeta) \
-    SIMPLE_VAR(Int_t, njetingap) \
-    SIMPLE_VAR(Double_t, mva) \
-    SIMPLE_VAR(Double_t, jdphi) \
-    SIMPLE_VAR(Double_t, dijetpt) \
-    SIMPLE_VAR(Double_t, dijetphi) \
-    SIMPLE_VAR(Double_t, hdijetphi) \
-    SIMPLE_VAR(Double_t, visjeteta) \
-    SIMPLE_VAR(Double_t, ptvis) \
-    SIMPLE_VAR(Int_t, nbtag) \
-    SIMPLE_VAR(Int_t, njets) \
-    SIMPLE_VAR(Int_t, njetspt20) \
-    /*trigger*/ \
-    SIMPLE_VAR(Int_t, l1TrigMatched_diTau) \
-    SIMPLE_VAR(Int_t, l2TrigMatched_diTau) \
-    SIMPLE_VAR(Int_t, l1TrigMatched_diTauJet) \
-    SIMPLE_VAR(Int_t, l2TrigMatched_diTauJet) \
-    SIMPLE_VAR(Int_t, jetTrigMatched_diTauJet) \
-    SIMPLE_VAR(Double_t, triggerWeight_diTauJet) \
-    SIMPLE_VAR(Double_t, triggerEffMC_diTauJet) \
-    SIMPLE_VAR(Double_t, triggerEffData_diTauJet) \
-    SIMPLE_VAR(Double_t, triggerEffMC_diTau) \
-    SIMPLE_VAR(Double_t, triggerEffData_diTau) \
-    SIMPLE_VAR(Double_t, triggerWeight_diTau) \
-    /**/ \
-    SIMPLE_VAR(Double_t, muon1Pt) \
-    SIMPLE_VAR(Double_t, electron1Pt) \
-    SIMPLE_VAR(Double_t, genfilter) \
-    SIMPLE_VAR(Double_t, tauspin) \
-    SIMPLE_VAR(Double_t, zmumusel) \
-    SIMPLE_VAR(Double_t, muradcorr) \
-    SIMPLE_VAR(Double_t, genTau2PtVsGenTau1Pt) \
-    SIMPLE_VAR(Double_t, genTau2EtaVsGenTau1Eta) \
-    SIMPLE_VAR(Double_t, genDiTauMassVsGenDiTauPt) \
+    SIMPLE_VAR(Int_t, run) /* Run */ \
+    SIMPLE_VAR(Int_t, lumi) /* Lumi */ \
+    SIMPLE_VAR(Int_t, evt) /* Evt */ \
+    /* Event Variables */ \
+    SIMPLE_VAR(Int_t, npv) /* NPV */ \
+    SIMPLE_VAR(Int_t, npu) /* NPU */ \
+    SIMPLE_VAR(Float_t, rho) /* Rho */ \
+    /* Event Weights */ \
+    SIMPLE_VAR(Float_t, mcweight) /* MC Weight (xs/nevents * additional wieght (ie pt weight for gghiggs)) */ \
+    SIMPLE_VAR(Float_t, puweight) /* Pielup Weight */ \
+    SIMPLE_VAR(Float_t, trigweight_1) /* Effieiency Scale factor (all components multiplied in) */ \
+    SIMPLE_VAR(Float_t, trigweight_2) /* Effieiency Scale factor (all components multiplied in) */ \
+    SIMPLE_VAR(Float_t, idweight_1) /* Effieiency Scale factor (all components multiplied in) */ \
+    SIMPLE_VAR(Float_t, idweight_2) /* Effieiency Scale factor (all components multiplied in) */ \
+    SIMPLE_VAR(Float_t, isoweight_1) /* Effieiency Scale factor (all components multiplied in) */ \
+    SIMPLE_VAR(Float_t, isoweight_2) /* Effieiency Scale factor (all components multiplied in) */ \
+    SIMPLE_VAR(Float_t, fakeweight) /* Effieiency Scale factor (all components multiplied in) */ \
+    SIMPLE_VAR(Float_t, effweight) /* Effieiency Scale factor (all components multiplied in) */ \
+    SIMPLE_VAR(Float_t, weight) /* mcweight*puweight*effweight */ \
+    SIMPLE_VAR(Float_t, embeddedWeight) /*  */ \
+    SIMPLE_VAR(Float_t, signalWeight) /*  */ \
+    /* SV Fit variables */ \
+    SIMPLE_VAR(Float_t, mvis) /* SV Fit using integration method */ \
+    SIMPLE_VAR(Float_t, m_sv) /* SV Fit using integration method */ \
+    SIMPLE_VAR(Float_t, pt_sv) /* SV Fit using integration method */ \
+    SIMPLE_VAR(Float_t, eta_sv) /* SV Fit using integration method */ \
+    SIMPLE_VAR(Float_t, phi_sv) /* SV Fit using integration method */ \
+    SIMPLE_VAR(Float_t, m_sv_Up) /* High Energy scale shape */ \
+    SIMPLE_VAR(Float_t, m_sv_Down) /* Low Energy Scale Shape */ \
+    /* First lepton :  muon for mu Tau, electron for e Tau, electron for e mu, Leading (in pT) Tau for Tau Tau */ \
+    SIMPLE_VAR(Float_t, pt_1) /* pT */ \
+    SIMPLE_VAR(Float_t, phi_1) /* Phi */ \
+    SIMPLE_VAR(Float_t, eta_1) /* Eta */ \
+    SIMPLE_VAR(Float_t, m_1) /* Mass */ \
+    SIMPLE_VAR(Int_t, q_1) /* Charge */ \
+    SIMPLE_VAR(Float_t, iso_1) /* Delta Beta iso value */ \
+    SIMPLE_VAR(Float_t, mva_1) /* MVA id (when using electron) 0 otherwise */ \
+    SIMPLE_VAR(Float_t, d0_1) /* d0 with respect to primary vertex */ \
+    SIMPLE_VAR(Float_t, dZ_1) /* dZ with respect to primary vertex */ \
+    SIMPLE_VAR(Bool_t, passid_1) /* Whether it passes id  (not necessarily iso) */ \
+    SIMPLE_VAR(Bool_t, passiso_1) /* Whether it passes iso (not necessarily id) */ \
+    SIMPLE_VAR(Float_t, mt_1) /* mT of  first lepton wrt to MVA met */ \
+    SIMPLE_VAR(Float_t, byCombinedIsolationDeltaBetaCorrRaw3Hits_1) /*  */ \
+    SIMPLE_VAR(Float_t, againstElectronMVA3raw_1) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    SIMPLE_VAR(Float_t, byIsolationMVA2raw_1) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    SIMPLE_VAR(Float_t, againstMuonLoose2_1) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    SIMPLE_VAR(Float_t, againstMuonMedium2_1) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    SIMPLE_VAR(Float_t, againstMuonTight2_1) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    /* Second lepton :  hadronic Tau for mu Tau had for e Tau, Muon for e mu, Trailing (in pT)  Tau for Tau Tau */ \
+    SIMPLE_VAR(Float_t, pt_2) /* pT */ \
+    SIMPLE_VAR(Float_t, phi_2) /* Phi */ \
+    SIMPLE_VAR(Float_t, eta_2) /* Eta */ \
+    SIMPLE_VAR(Float_t, m_2) /* Mass */ \
+    SIMPLE_VAR(Int_t, q_2) /* Charge */ \
+    SIMPLE_VAR(Float_t, iso_2) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    SIMPLE_VAR(Float_t, d0_2) /* d0 with respect to primary vertex */ \
+    SIMPLE_VAR(Float_t, dZ_2) /* dZ with respect to primary vertex */ \
+    SIMPLE_VAR(Float_t, mva_2) /* MVA id (for anti electron id) */ \
+    SIMPLE_VAR(Bool_t, passid_2) /* Whether it passes id  (not necessarily iso) */ \
+    SIMPLE_VAR(Bool_t, passiso_2) /* Whether it passes iso (not necessarily id) */ \
+    SIMPLE_VAR(Float_t, mt_2) /* mT of 2nd lepton wrt to MVA met */ \
+    SIMPLE_VAR(Float_t, byCombinedIsolationDeltaBetaCorrRaw3Hits_2) /*  */ \
+    SIMPLE_VAR(Float_t, againstElectronMVA3raw_2) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    SIMPLE_VAR(Float_t, byIsolationMVA2raw_2) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    SIMPLE_VAR(Float_t, againstMuonLoose2_2) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    SIMPLE_VAR(Float_t, againstMuonMedium2_2) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    SIMPLE_VAR(Float_t, againstMuonTight2_2) /* MVA iso for hadronic Tau, Delta Beta for muon */ \
+    /* Di-lepton */ \
+    SIMPLE_VAR(Float_t, pt_tt) /* pT */ \
+    /* Met related variables */ \
+    SIMPLE_VAR(Float_t, met) /* pfmet */ \
+    SIMPLE_VAR(Float_t, metphi) /* pfmet Phi */ \
+    SIMPLE_VAR(Float_t, mvamet) /* mvamet */ \
+    SIMPLE_VAR(Float_t, mvametphi) /* mvamet Phi */ \
+    SIMPLE_VAR(Float_t, pzetavis) /* pZeta Visible */ \
+    SIMPLE_VAR(Float_t, pzetamiss) /* pZeta Missing */ \
+    /* MET covariance matrices */ \
+    SIMPLE_VAR(Float_t, metcov00) /* pf met covariance matrix 00 */ \
+    SIMPLE_VAR(Float_t, metcov01) /* pf met covariance matrix 01 */ \
+    SIMPLE_VAR(Float_t, metcov10) /* pf met covariance matrix 10 */ \
+    SIMPLE_VAR(Float_t, metcov11) /* pf met covariance matrix 11 */ \
+    /* MVAMet covariance matrices */ \
+    SIMPLE_VAR(Float_t, mvacov00) /* mva met covariance matrix 00 */ \
+    SIMPLE_VAR(Float_t, mvacov01) /* mva met covariance matrix 01 */ \
+    SIMPLE_VAR(Float_t, mvacov10) /* mva met covariance matrix 10 */ \
+    SIMPLE_VAR(Float_t, mvacov11) /* mva met covariance matrix 11 */ \
+    /* First Jet   : leading jet after applying Jet energy corrections (excluding hadronic Tau) */ \
+    SIMPLE_VAR(Float_t, jpt_1) /* Jet Pt after corrections */ \
+    SIMPLE_VAR(Float_t, jeta_1) /* Jet Eta */ \
+    SIMPLE_VAR(Float_t, jphi_1) /* Jet Phi */ \
+    SIMPLE_VAR(Float_t, jptraw_1) /* Jet Raw Pt (before corrections) */ \
+    SIMPLE_VAR(Float_t, jptunc_1) /* Jet Unc (relative to Jet corrected pT) */ \
+    SIMPLE_VAR(Float_t, jmva_1) /* Jet MVA id value */ \
+    SIMPLE_VAR(Float_t, jlrm_1) /* Jet MVA id value */ \
+    SIMPLE_VAR(Int_t, jctm_1) /* Jet MVA id value */ \
+    SIMPLE_VAR(Bool_t, jpass_1) /* Whether Jet pass PU Id Loose WP */ \
+    /* Second Jet  : 2nd leading jet (in pt) afer applying Jet energy corrections (excluding Tau) */ \
+    SIMPLE_VAR(Float_t, jpt_2) /* Jet Pt after corrections */ \
+    SIMPLE_VAR(Float_t, jeta_2) /* Jet Eta */ \
+    SIMPLE_VAR(Float_t, jphi_2) /* Jet Phi */ \
+    SIMPLE_VAR(Float_t, jptraw_2) /* Jet Raw Pt (before corrections) */ \
+    SIMPLE_VAR(Float_t, jptunc_2) /* Jet Unc (relative to Jet corrected pT) */ \
+    SIMPLE_VAR(Float_t, jmva_2) /* Jet MVA id value */ \
+    SIMPLE_VAR(Float_t, jlrm_2) /* Jet MVA id value */ \
+    SIMPLE_VAR(Int_t, jctm_2) /* Jet MVA id value */ \
+    SIMPLE_VAR(Bool_t, jpass_2) /* Whether jet passes PU Id Loose WP */ \
+    /* Candidate B Jets : leading jet (in CSV ordering) passing (pt > 20 + eta < 2.4) */ \
+    SIMPLE_VAR(Float_t, bpt_1) /* Corrected BTag Pt */ \
+    SIMPLE_VAR(Float_t, beta_1) /* Btag Eta */ \
+    SIMPLE_VAR(Float_t, bphi_1) /* Btag Phi */ \
+    SIMPLE_VAR(Float_t, bcsv_1) /* Btag CSV */ \
+    /* Candidate B Jets : subleading jet (in CSV ordering) passing (pt > 20 + eta < 2.4) */ \
+    SIMPLE_VAR(Float_t, bpt_2) /* Corrected BTag Pt */ \
+    SIMPLE_VAR(Float_t, beta_2) /* Btag Eta */ \
+    SIMPLE_VAR(Float_t, bphi_2) /* Btag Phi */ \
+    SIMPLE_VAR(Float_t, bcsv_2) /* Btag CSV */ \
+    /* Candidate B Jets : third jet (in CSV ordering) passing (pt > 20 + eta < 2.4) */ \
+    SIMPLE_VAR(Float_t, bpt_3) /* Corrected BTag Pt */ \
+    SIMPLE_VAR(Float_t, beta_3) /* Btag Eta */ \
+    SIMPLE_VAR(Float_t, bphi_3) /* Btag Phi */ \
+    SIMPLE_VAR(Float_t, bcsv_3) /* Btag CSV */ \
+    /* number of btags passing btag id (medium CSV WP) ( pt > 20 ) */ \
+    SIMPLE_VAR(Int_t, nbtag) /*  */ \
+    /* number of jets passing jet id ( pt > 30 ) */ \
+    SIMPLE_VAR(Int_t, njets) /*  */ \
+    SIMPLE_VAR(Int_t, njetspt20) /*  */ \
     /**/
 
 #define SIMPLE_VAR(type, name) DECLARE_SIMPLE_BRANCH_VARIABLE(type, name)
