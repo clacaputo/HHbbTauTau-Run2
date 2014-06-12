61f40b099059d6b5f3ac3c0395dc7a0942598458
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-06-12 10:32:30 +0200

    Modified Print_SyncPlots.
    - Fixed bug with last page title.
    - Print all relevant histograms.
    - For each printed value, if difference between mine and other is
      greater than 10%, report the event and the difference.

--- Analysis/source/HmutauBaseline_sync.C
+++ Analysis/source/HmutauBaseline_sync.C
@@ -119,12 +119,13 @@ protected:
         //postRecoilMET = correctedMET; //tau corrections
 
         const Candidate higgs_sv = CorrectMassBySVfit(higgs, postRecoilMET,1);
-        const Candidate higgs_sv_up = CorrectMassBySVfit(higgs, postRecoilMET,1.03);
-        const Candidate higgs_sv_down = CorrectMassBySVfit(higgs, postRecoilMET,0.97);
+//        const Candidate higgs_sv_up = CorrectMassBySVfit(higgs, postRecoilMET,1.03);
+//        const Candidate higgs_sv_down = CorrectMassBySVfit(higgs, postRecoilMET,0.97);
 
         CalculateFullEventWeight(higgs_sv);
 
-        FillSyncTree(higgs, higgs_sv, higgs_sv_up, higgs_sv_down, filteredJets, jetsPt20, bjets, vertices);
+//        FillSyncTree(higgs, higgs_sv, higgs_sv_up, higgs_sv_down, filteredJets, jetsPt20, bjets, vertices);
+        FillSyncTree(higgs, higgs_sv, higgs_sv, higgs_sv, filteredJets, jetsPt20, bjets, vertices);
 
         //postRecoilMET = mvaMet;
 //        postRecoilMET = correctedMET; //tau corrections
