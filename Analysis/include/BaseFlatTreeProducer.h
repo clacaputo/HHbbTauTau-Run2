d17c0dc3a1613afaeba55247cc18821e6e4eb85f
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-09-20 15:31:26 +0200

    Added MSSM_A to the SM_resonance list.
    Hotfix to be compatible with legacy MSSM gg_bbH_bbtautau samples where H
    has code 36...

--- Analysis/include/BaseFlatTreeProducer.h
+++ Analysis/include/BaseFlatTreeProducer.h
@@ -211,7 +211,8 @@ protected:
         static const particles::ParticleCodes resonanceCodes = { particles::MSSM_H, particles::MSSM_A };
         static const particles::ParticleCodes resonanceDecay_1 = { particles::Higgs, particles::Higgs };
         static const particles::ParticleCodes resonanceDecay_2 = { particles::Z, particles::Higgs };
-        static const particles::ParticleCodes SM_ResonanceCodes = { particles::Higgs, particles::Z, particles::MSSM_H };
+        static const particles::ParticleCodes SM_ResonanceCodes = { particles::Higgs, particles::Z,
+                                                                    particles::MSSM_H, particles::MSSM_A };
         static const particles::ParticleCodes SM_ResonanceDecay_1 = { particles::tau, particles::tau };
         static const particles::ParticleCodes SM_ResonanceDecay_2 = { particles::b, particles::b };
         static const particles::ParticleCodes2D HiggsDecays = { SM_ResonanceDecay_1, SM_ResonanceDecay_2 };
