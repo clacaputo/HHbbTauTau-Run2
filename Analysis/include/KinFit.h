4b29c74d1ae2639a13b8eb7aa247622bece41340
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-09-21 19:32:33 +0200

    First try for standard CMSSW KinFitter package.
    - Analysis/include/KinFit.h: first version of two-body fit take from an
      example.
    - RunTools/make.sh: added boost_thread library to be compatible with
      CMSSW.

--- Analysis/include/KinFit.h
+++ Analysis/include/KinFit.h
@@ -36,6 +36,44 @@
 #include "HHKinFit/source/PSMath.cpp"
 #include "HHKinFit/source/PSTools.cpp"
 
+
+
+
+#include "FWCore/Utilities/src/EDMException.cc"
+#include "FWCore/Utilities/src/Exception.cc"
+
+#include "FWCore/MessageLogger/src/MessageLogger.cc"
+#include "FWCore/MessageLogger/src/MessageLoggerQ.cc"
+#include "FWCore/MessageLogger/src/MessageDrop.cc"
+#include "FWCore/MessageLogger/src/ELseverityLevel.cc"
+#include "FWCore/MessageLogger/src/MessageSender.cc"
+#include "FWCore/MessageLogger/src/AbstractMLscribe.cc"
+#include "FWCore/MessageLogger/src/ErrorObj.cc"
+#include "FWCore/MessageLogger/src/ELextendedID.cc"
+#include "FWCore/MessageLogger/src/ELstring.cc"
+
+using namespace std;
+#include "PhysicsTools/KinFitter/src/TAbsFitConstraint.cc"
+#include "PhysicsTools/KinFitter/src/TAbsFitParticle.cc"
+#include "PhysicsTools/KinFitter/src/TFitConstraintEp.cc"
+#include "PhysicsTools/KinFitter/src/TFitConstraintM.cc"
+#include "PhysicsTools/KinFitter/src/TFitConstraintMGaus.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleCart.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleECart.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleEMomDev.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleEScaledMomDev.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleESpher.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleEtEtaPhi.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleEtThetaPhi.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleMCCart.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleMCMomDev.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleMCPInvSpher.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleMCSpher.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleMomDev.cc"
+#include "PhysicsTools/KinFitter/src/TFitParticleSpher.cc"
+#include "PhysicsTools/KinFitter/src/TKinFitter.cc"
+#include "PhysicsTools/KinFitter/src/TSLToyGen.cc"
+
 #include "TreeProduction/interface/MET.h"
 
 #include "AnalysisBase/include/Candidate.h"
@@ -160,6 +198,105 @@ inline FitResults FourBodyFit(const FourBodyFitInput& input)
     return result;
 }
 
+inline Double_t ErrEta(Float_t Et, Float_t Eta)
+{
+    Double_t InvPerr2, a, b, c;
+    if(fabs(Eta) < 1.4){
+        a = 1.215;
+        b = 0.037;
+        c = 7.941 * 0.0001;
+    } else {
+        a = 1.773;
+        b = 0.034;
+        c = 3.56 * 0.0001;
+    }
+    InvPerr2 = a/(Et * Et) + b/Et + c;
+    return InvPerr2;
+}
+
+inline Double_t ErrEt(Float_t Et, Float_t Eta)
+{
+    Double_t InvPerr2, a, b, c;
+      if(fabs(Eta) < 1.4){
+        a = 5.6;
+    b = 1.25;
+        c = 0.033;
+      }else{
+        a = 4.8;
+        b = 0.89;
+        c = 0.043;
+    }
+    InvPerr2 = (a * a) + (b * b) * Et + (c * c) * Et * Et;
+    return InvPerr2;
+}
+
+inline Double_t ErrPhi(Float_t Et, Float_t Eta)
+{
+Double_t InvPerr2, a, b, c;
+  if(fabs(Eta) < 1.4){
+    a = 6.65;
+    b = 0.04;
+    c = 8.49 * 0.00001;
+  }else{
+      a = 2.908;
+      b = 0.021;
+      c = 2.59 * 0.0001;
+  }
+InvPerr2 = a/(Et * Et) + b/Et + c;
+  return InvPerr2;
+}
+
+
+inline FitResults TwoBodyFit(const TLorentzVector& momentum_1, const TLorentzVector& momentum_2)
+{
+    FitResults result;
+    TMatrixD m1(3, 3);
+    TMatrixD m2(3, 3);
+    m1.Zero();
+    m2.Zero();
+
+    m1(0, 0) = ErrEt(momentum_1.Et(), momentum_1.Eta());
+    m1(1, 1) = ErrEta(momentum_1.Et(), momentum_1.Eta());
+    m1(2, 2) = ErrPhi(momentum_1.Et(), momentum_1.Eta());
+
+    m2(0, 0) = ErrEt(momentum_2.Et(), momentum_2.Eta());
+    m2(1, 1) = ErrEta(momentum_2.Et(), momentum_2.Eta());
+    m2(2, 2) = ErrPhi(momentum_2.Et(), momentum_2.Eta());
+
+    TLorentzVector mom1(momentum_1), mom2(momentum_2);
+    TFitParticleEtEtaPhi _jet1(&mom1, &m1);
+    TFitParticleEtEtaPhi _jet2(&mom2, &m2);
+
+    TFitConstraintM m_bb;
+    m_bb.addParticle1(&_jet1);
+    m_bb.addParticle1(&_jet2);
+    m_bb.setMassConstraint(125.0);
+
+    TKinFitter _fitter;
+    _fitter.addMeasParticle(&_jet1);
+    _fitter.addMeasParticle(&_jet2);
+    _fitter.addConstraint(&m_bb);
+
+    _fitter.setMaxNbIter(30);
+    _fitter.setMaxDeltaS(1e-2);
+    _fitter.setMaxF(1e-1);
+    _fitter.setVerbosity(0);
+    const Int_t fit_result = _fitter.fit();
+
+    std::cout << "fit result: " << fit_result << std::endl;
+    if(fit_result == 0) {
+        std::cout << "Old 1: " << momentum_1 << std::endl;
+        std::cout << "Old 2: " << momentum_2 << std::endl;
+        std::cout << "New 1: " << *_jet1.getCurr4Vec() << std::endl;
+        std::cout << "New 2: " << *_jet2.getCurr4Vec() << std::endl;
+        const double m_old = (momentum_1 + momentum_2).M();
+        const double m_new = ((*_jet1.getCurr4Vec()) + (*_jet2.getCurr4Vec())).M();
+        std::cout << "M old: " << m_old << std::endl;
+        std::cout << "M new: " << m_new << std::endl;
+    }
+    return result;
+}
+
 inline FitResultsWithUncertainties FitWithUncertainties(const FourBodyFitInput& input, double bjet_energy_uncertainty,
                                                         double tau_energy_uncertainty, bool fit_two_bjets,
                                                         bool fit_four_body)
@@ -176,6 +313,7 @@ inline FitResultsWithUncertainties FitWithUncertainties(const FourBodyFitInput& 
         result.fit_bb_up_tt_up = FourBodyFit(FourBodyFitInput(input, 1 + bjet_energy_uncertainty,
                                                               1 + tau_energy_uncertainty));
     }
+    TwoBodyFit(input.bjet_momentums.at(0), input.bjet_momentums.at(1));
     return result;
 }
 
