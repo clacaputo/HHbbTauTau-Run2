8c14959e2d42ca2b00c2ab70333e653c827aa05d
Maria Teresa <grippomariateresa@gmail.com>
2014-09-24 12:43:25 +0200

    MVA training:
    - MVASelections/include/MVA_common.h: added method TMVAtestAndTraining
    - MVASelections/source/MVA_etau.C/MVA_mutau.C/MVA_tautau.C: modified to
      have a common structure, the only thing which changes is selection

--- MVASelections/include/MVA_common.h
+++ MVASelections/include/MVA_common.h
@@ -2,6 +2,8 @@
 
 #include <TChain.h>
 #include <TH1.h>
+#include "TMVA/Factory.h"
+#include "TMVA/Tools.h"
 
 struct FlatTree {
     TChain* chain;
@@ -14,7 +16,8 @@ struct FlatTree {
 
     Int_t nBjet;
     Float_t mvamet_phi, met, drLT, ptsv, etasv, phisv;
-    Float_t pt1, pt2, eta1, eta2, phi1, phi2, e1, e2,byCombinedIsolationDeltaBetaCorrRaw3Hits2,pfRelIso1,mt1,mt2,weight;
+    Float_t pt1, pt2, eta1, eta2, phi1, phi2, e1, e2;
+    Float_t byCombinedIsolationDeltaBetaCorrRaw3Hits1,byCombinedIsolationDeltaBetaCorrRaw3Hits2,pfRelIso1,mt1,mt2,weight;
     Int_t   Q1,Q2;
     Bool_t  againstMuonTight2,againstElectronLooseMVA2;
 
@@ -45,6 +48,7 @@ struct FlatTree {
 
         chain->SetBranchAddress("againstMuonTight_2",&againstMuonTight2);
         chain->SetBranchAddress("againstElectronLooseMVA_2",&againstElectronLooseMVA2);
+        chain->SetBranchAddress("byCombinedIsolationDeltaBetaCorrRaw3Hits_1",&byCombinedIsolationDeltaBetaCorrRaw3Hits1);
         chain->SetBranchAddress("byCombinedIsolationDeltaBetaCorrRaw3Hits_2",&byCombinedIsolationDeltaBetaCorrRaw3Hits2);
 
         chain->SetBranchAddress("pfRelIso_1",&pfRelIso1);
@@ -66,5 +70,47 @@ inline TH1F* MakeTH1F(const std::string& name, const std::string& prefix, Int_t 
 {
     const std::string full_name = name + "_" + prefix;
     return new TH1F(full_name.c_str(), full_name.c_str(), nbinsx, xlow, xup);
+}
+
+void TMVAtestAndTraining(TMVA::Factory *factory)
+{
+    TCut mycuts = "";
+    TCut mycutb = "";
+
+    factory->PrepareTrainingAndTestTree(mycuts, mycutb,"SplitMode=Random" );
+
+    cout<<"*******************************************Call BDT***************************************"<<endl;
+    // Adaptive Boost
+//	factory->BookMethod( TMVA::Types::kBDT, "BDT",
+//						"!H:!V:NTrees=850:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:SeparationType=GiniIndex:nCuts=20" );
+
+
+    factory->BookMethod( TMVA::Types::kBDT, "BDT",
+                        "!H:!V:NTrees=850:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:SeparationType=GiniIndex:nCuts=20" );
+
+
+    cout<<"**************************************	Call Fisher******************************************"<<endl;
+
+    factory->BookMethod( TMVA::Types::kBDT, "BDTMitFisher",
+                            "!H:!V:NTrees=50:UseFisherCuts:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:SeparationType=GiniIndex:nCuts=20" );
+
+    cout<<"********************************************************************************"<<endl;
+
+    cout<<"**************************************	Call Decorrelation + AdaBoost******************************************"<<endl;
+    // Decorrelation + Adaptive Boost
+    factory->BookMethod( TMVA::Types::kBDT, "BDTD","!H:!V:NTrees=400:MaxDepth=3:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=20:VarTransform=Decorrelate" );
+    cout<<"********************************************************************************"<<endl;
+
+
+    // Train MVAs using the set of training events
+    factory->TrainAllMethods();
+
+    // ---- Evaluate all MVAs using the set of test events
+    factory->TestAllMethods();
+
+    // ----- Evaluate and compare performance of all configured MVAs
+    factory->EvaluateAllMethods();
+
+    // --------------------------------------------------------------
 }
 
