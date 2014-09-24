641b0cdb9463b85f6f356661bb08a4d6dcdd8c67
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-09-24 17:10:34 +0200

    Continued refactoring of MVASelections code.
    - Extracted common functionality to MVA_common.h.
    - Added file weights for signal and background.
    - Made code compilable with clang.
    - Currently only MVA_tautau is working.

--- MVASelections/include/MVA_common.h
+++ MVASelections/include/MVA_common.h
@@ -2,8 +2,12 @@
 
 #include <TChain.h>
 #include <TH1.h>
-#include "TMVA/Factory.h"
-#include "TMVA/Tools.h"
+#include <TMVA/Factory.h>
+#include <TMVA/Tools.h>
+#include <TLorentzVector.h>
+
+
+typedef std::vector<size_t> IndexVector;
 
 struct FlatTree {
     TChain* chain;
@@ -14,8 +18,7 @@ struct FlatTree {
     std::vector<Float_t> *energy_BJets;
     std::vector<Float_t> *csv_BJets;
 
-    Int_t nBjet;
-    Float_t mvamet_phi, met, drLT, ptsv, etasv, phisv;
+    Float_t mvamet_phi, mvamet, drLT, ptsv, etasv, phisv;
     Float_t pt1, pt2, eta1, eta2, phi1, phi2, e1, e2;
     Float_t byCombinedIsolationDeltaBetaCorrRaw3Hits1,byCombinedIsolationDeltaBetaCorrRaw3Hits2,pfRelIso1,mt1,mt2,weight;
     Int_t   Q1,Q2;
@@ -38,9 +41,8 @@ struct FlatTree {
         chain->SetBranchAddress( "phi_Bjets", &phi_BJets);
         chain->SetBranchAddress( "energy_Bjets", &energy_BJets);
         chain->SetBranchAddress( "csv_Bjets", &csv_BJets);
-        chain->SetBranchAddress("njetspt20", &nBjet);
         chain->SetBranchAddress("mvametphi", &mvamet_phi);
-        chain->SetBranchAddress("mvamet", &met);
+        chain->SetBranchAddress("mvamet", &mvamet);
         chain->SetBranchAddress("DeltaR_leptons", &drLT);
         chain->SetBranchAddress("pt_sv", &ptsv);
         chain->SetBranchAddress("eta_sv", &etasv);
@@ -64,12 +66,188 @@ struct FlatTree {
     Int_t Add(const char* name) { return chain->Add(name); }
     Long64_t GetEntriesFast() const { return chain->GetEntriesFast(); }
     Int_t GetEntry(Long64_t entry) { return chain->GetEntry(entry); }
+    std::string GetShortFileName()
+    {
+        const std::string full_file_name = chain->GetFile()->GetName();
+        const size_t pos = full_file_name.find_last_of('/');
+        if(pos == std::string::npos)
+            return full_file_name;
+        return full_file_name.substr(pos + 1);
+    }
+
+    IndexVector CollectJets() const
+    {
+        IndexVector jet_indexes;
+        for(size_t k = 0; k < eta_BJets->size(); ++k) {
+            if(TMath::Abs(eta_BJets->at(k)) < 2.4)
+                jet_indexes.push_back(k);
+        }
+        return jet_indexes;
+    }
+
+    IndexVector CollectMediumBJets() const
+    {
+        IndexVector jet_indexes;
+        for(size_t k = 0; k < eta_BJets->size(); ++k) {
+            if(TMath::Abs(eta_BJets->at(k)) < 2.4 && csv_BJets->at(k) > 0.679)
+                jet_indexes.push_back(k);
+        }
+        return jet_indexes;
+    }
+
+    TLorentzVector GetBJetMomentum(size_t bjet_index) const
+    {
+        TLorentzVector momentum;
+        momentum.SetPtEtaPhiE(pt_BJets->at(bjet_index), eta_BJets->at(bjet_index),
+                              phi_BJets->at(bjet_index), energy_BJets->at(bjet_index));
+        return momentum;
+    }
+
+    TLorentzVector GetLeptonMomentum(size_t lepton_id) const
+    {
+        TLorentzVector momentum;
+        if(lepton_id == 1)
+            momentum.SetPtEtaPhiE(pt1, eta1, phi1, e1);
+        else if(lepton_id == 2)
+            momentum.SetPtEtaPhiE(pt2, eta2, phi2, e2);
+        else
+            throw std::runtime_error("Unknown lepton id");
+        return momentum;
+    }
+
+    TLorentzVector GetMetMomentum() const
+    {
+        TLorentzVector momentum;
+        momentum.SetPtEtaPhiE(mvamet, 0, mvamet_phi, mvamet);
+        return momentum;
+    }
 };
 
 inline TH1F* MakeTH1F(const std::string& name, const std::string& prefix, Int_t nbinsx,Double_t xlow,Double_t xup)
 {
     const std::string full_name = name + "_" + prefix;
     return new TH1F(full_name.c_str(), full_name.c_str(), nbinsx, xlow, xup);
+}
+
+class TH1F_Ptr {
+public:
+    TH1F_Ptr() : hist(0) {}
+    TH1F_Ptr(TH1F* _hist) : hist(_hist) {}
+    TH1F* operator-> () { return hist; }
+
+    TH1F_Ptr& operator= (TH1F* _hist)
+    {
+        if(hist) {
+
+            hist->Write();
+            delete hist;
+        }
+        hist = _hist;
+        return *this;
+    }
+
+    ~TH1F_Ptr()
+    {
+        if(hist) {
+            hist->Write();
+            delete hist;
+        }
+    }
+
+private:
+    TH1F_Ptr(const TH1F_Ptr& /*other*/) { }
+    TH1F_Ptr& operator= (const TH1F_Ptr& /*other*/) { return *this; }
+
+private:
+    TH1F* hist;
+};
+
+struct MVA_Histograms {
+    TH1F_Ptr pt_l1, pt_l2, pt_b1, pt_b2, dR_bb, dPhi_bb_MET, dR_ll,
+             pt_Htt, dR_Hbb_Htt, pt_Hbb, dPhi_MET_tt, pt_H, mT2;
+
+    MVA_Histograms(bool is_signal)
+    {
+        const std::string prefix = is_signal ? "signal" : "bkg";
+        pt_l1 = MakeTH1F("hPt1",prefix,200,0,200);
+        pt_l2 = MakeTH1F("hPt2",prefix,200,0,200);
+        pt_b1 = MakeTH1F("hPtb1",prefix,300, 0, 300);
+        pt_b2 = MakeTH1F("hPtb2",prefix,300, 0, 300);
+        dR_bb = MakeTH1F("hDRbb",prefix,100, 0, 10);
+        dPhi_bb_MET = MakeTH1F("hDPhiBBMET",prefix,100, -3, 3);
+        dR_ll = MakeTH1F("hDRll",prefix,100, 0, 10);
+        pt_Htt = MakeTH1F("hPtHtt",prefix,600, 0, 600);
+        dR_Hbb_Htt= MakeTH1F("hDRHBBTT",prefix,100, 0, 10);
+        pt_Hbb= MakeTH1F("hPtHBB",prefix,600, 0, 600);
+        dPhi_MET_tt= MakeTH1F("hDeltaPhi_METTT",prefix,100, -3, 3);
+        pt_H= MakeTH1F("hPtH",prefix,1000, 0, 1000);
+        mT2= MakeTH1F("hmT2",prefix,600, 0, 600);
+    }
+};
+
+inline void AddEvent(TMVA::Factory *factory, const std::vector<Double_t>& vars, bool is_signal, Double_t weight)
+{
+    if (gRandom->Uniform(0, 1) < 0.5) {
+        if(is_signal)
+            factory->AddSignalTrainingEvent(vars, weight);
+        else
+            factory->AddBackgroundTrainingEvent(vars, weight);
+    } else {
+        if(is_signal)
+            factory->AddSignalTestEvent(vars, weight);
+        else
+            factory->AddBackgroundTestEvent(vars, weight);
+    }
+}
+
+inline Double_t GetFileScaleFactor(const std::string& file_name)
+{
+    static std::map<std::string, Double_t> file_name_map;
+    if(!file_name_map.size()) {
+        file_name_map["ggH_hh_bbtautau_260.root"] = 0.0358936;
+        file_name_map["ggH_hh_bbtautau_270.root"] = 0.03057;
+        file_name_map["ggH_hh_bbtautau_280.root"] = 0.0284073;
+        file_name_map["ggH_hh_bbtautau_290.root"] = 0.0201117;
+        file_name_map["ggH_hh_bbtautau_300.root"] = 0.0253141;
+        file_name_map["ggH_hh_bbtautau_310.root"] = 0.0221424;
+        file_name_map["ggH_hh_bbtautau_320.root"] = 0.0215317;
+        file_name_map["ggH_hh_bbtautau_330.root"] = 0.0199818;
+        file_name_map["ggH_hh_bbtautau_340.root"] = 0.0207792;
+        file_name_map["ggH_hh_bbtautau_350.root"] = 0.0179311;
+
+        file_name_map["tt_hadr.root"] = 0.0719394;
+        file_name_map["tt_lept.root"] = 0.0429666;
+        file_name_map["tt_semi.root"] = 0.0862387;
+    }
+    if(!file_name_map.count(file_name)) {
+        std::cerr << "ERROR: unknown file name: " << file_name << std::endl;
+        throw std::runtime_error("Unknown file name.");
+    }
+    return file_name_map[file_name];
+}
+
+bool ApplyFullEventSelection(const FlatTree* tree, std::vector<Double_t>& vars, MVA_Histograms& h);
+
+void ApplySelection(TMVA::Factory *factory, FlatTree* tree, bool is_signal)
+{
+    MVA_Histograms h(is_signal);
+
+    std::string file_name;
+    double file_scale_factor;
+
+    for (Long64_t i = 0; i < tree->GetEntriesFast(); i++) {
+        tree->GetEntry(i);
+        std::string current_file_name = tree->GetShortFileName();
+        if(file_name != current_file_name) {
+            file_scale_factor = GetFileScaleFactor(current_file_name);
+            std::cout << "New file: " << current_file_name << ", SF = " << file_scale_factor << std::endl;
+            file_name = current_file_name;
+        }
+        std::vector<Double_t> vars;
+        const bool event_passed = ApplyFullEventSelection(tree, vars, h);
+        if(event_passed)
+            AddEvent(factory, vars, is_signal, tree->weight * file_scale_factor);
+    }
 }
 
 void TMVAtestAndTraining(TMVA::Factory *factory)
