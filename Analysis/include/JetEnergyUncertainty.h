51e3e88114888b35ce91c68cfec94ee8c985d7b7
Konstantin Androsov <konstantin.androsov@gmail.com>
2014-11-17 10:17:36 +0100

    Implemented jet energy uncertainties estimation.
    - Analysis/config/*.cfg: added file with jet uncert and section to be
      used.
    - Analysis/data/Summer13_V5_Uncertainties: added from txt files with
      uncertainties from the twiki.
    - Analysis/include/BaseAnalyzer.h: do jet energy variation for events.
    - Analysis/include/Config.h: added JetEnergyUncertainties_inputFile and
      JetEnergyUncertainties_inputSection.
    - Analysis/include/JetEnergyUncertainty.h:
      - implemented ApplyCorrection following twiki recommendations;
      - added method CmsswWarningsWorkaround to work around warning for unsused
        functions declared in CMSSW include files.

--- Analysis/include/JetEnergyUncertainty.h
+++ Analysis/include/JetEnergyUncertainty.h
@@ -26,29 +26,53 @@
 
 #pragma once
 
+#include <TLorentzVector.h>
+
+#include "FWCore/Utilities/src/EDMException.cc"
+#include "FWCore/Utilities/src/Exception.cc"
+#include "FWCore/Utilities/src/typelookup.cc"
+
 #include "CondFormats/JetMETObjects/src/Utilities.cc"
 #include "CondFormats/JetMETObjects/src/JetCorrectorParameters.cc"
 #include "CondFormats/JetMETObjects/src/SimpleJetCorrectionUncertainty.cc"
 #include "CondFormats/JetMETObjects/src/JetCorrectionUncertainty.cc"
 
 #include "TreeProduction/interface/Jet.h"
+#include "AnalysisBase/include/AnalysisMath.h"
 
 namespace analysis {
 
 class JetEnergyUncertaintyCorrector {
 public:
-    JetEnergyUncertaintyCorrector(const std::string& input_file_name)
-        : parameters(input_file_name, "Total"), jetCorrector(parameters)
-    {
+    JetEnergyUncertaintyCorrector(const std::string& input_file_name, const std::string& section_name)
+        : parameters(input_file_name, section_name), jetCorrector(parameters) {}
 
+    void ApplyCorrection(ntuple::Jet& jet, bool scale_up)
+    {
+        jetCorrector.setJetPt(jet.pt);
+        jetCorrector.setJetEta(jet.eta);
+        const double uncertainty = jetCorrector.getUncertainty(scale_up);
+        const double sign = scale_up ? +1 : -1;
+        const double sf = 1.0 + sign * uncertainty;
+        const TLorentzVector original_momentum = MakeLorentzVectorPtEtaPhiM(jet.pt, jet.eta, jet.phi, jet.mass);
+        const TLorentzVector corrected_momentum = original_momentum * sf;
+        jet.pt = corrected_momentum.Pt();
+        jet.eta = corrected_momentum.Eta();
+        jet.phi = corrected_momentum.Phi();
+        jet.mass = corrected_momentum.M();
     }
 
-    ntuple::Jet ApplyCorrection(const ntuple::Jet& original_jet, bool scale_up)
+    void ApplyCorrection(ntuple::JetVector& jets, bool scale_up)
     {
-        ntuple::Jet corrected_jet = original_jet;
-        jetCorrector.setJetPt(original_jet.pt);
-        jetCorrector.setJetEta(original_jet.eta);
-        const double uncer
+        for(ntuple::Jet& jet : jets)
+            ApplyCorrection(jet, scale_up);
+    }
+
+private:
+    void CmsswWarningsWorkaround() const
+    {
+        float fx[0], fy[0];
+        quadraticInterpolation(0, fx, fy);
     }
 
 private:
