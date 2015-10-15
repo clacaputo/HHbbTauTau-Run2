f291f6b674cd37d082709fc2bb903ac53bf6f7bd
Claudio <cld.cpt@gmail.com>
2015-10-15 17:00:33 +0200

    Added new folder for microAOD production
    microAODProduction--.
    		    |--- crab/     will contain crab3 config files
    	            |--- python/   cfg for skimming, EleID and sumple ntupler
                        |--- plugins/  SkimFilter and ntupler
    
    Restore the old content of the PatProduction folder

--- PatProduction/plugins/SkimFilter.cc
+++ PatProduction/plugins/SkimFilter.cc
@@ -48,7 +48,6 @@ private:
     virtual bool filter(edm::Event&, const edm::EventSetup&);
 
 private:
-//    const edm::EDGetTokenT<reco::VertexCollection> vertexTag;
     edm::InputTag vertexTag;
     edm::InputTag muonTag;
     edm::InputTag electronTag;
@@ -57,9 +56,9 @@ private:
 
 bool SkimFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-    edm::Handle<reco::VertexCollection> hVertices;
+    edm::Handle<pat::VertexCollection> hVertices;
     iEvent.getByLabel(vertexTag, hVertices);
-    const reco::VertexCollection& vertices = *hVertices;
+    const pat::VertexCollection& vertices = *hVertices.product();
 
     edm::Handle<pat::MuonCollection> hMuons;
     iEvent.getByLabel(muonTag, hMuons);
@@ -76,46 +75,27 @@ bool SkimFilter::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
     bool haveGoodVertex = false;
     unsigned nGoodTaus = 0;
 
-    auto vtx = vertices.at(0);
-
-    for (const reco::Vertex& vertex : vertices){
+    for (const pat::Vertex& vertex : vertices){
         if(vertex.ndof() > 4 && std::abs(vertex.z()) < 24 && vertex.position().rho() < 2){
             haveGoodVertex = true;
             break;
         }
     }
-    if (!haveGoodVertex) {
-        std::cout<<"Event Dumped !!!!!!!"<<std::endl;
-        return false;}
-    std::cout<<"Prima .........."<<std::endl;
+    if (!haveGoodVertex) return false;
+
     for(const pat::Tau& tau : taus) {
-        if(tau.pt() > 18 && std::abs(tau.eta()) < 2.3  /*&& tau.tauID("decayModeFinding") > 0.5*/)
+        if(tau.pt() > 15.0 && std::abs(tau.eta()) < 2.3  && tau.tauID("decayModeFinding") > 0.5)
             ++nGoodTaus;
         if(nGoodTaus >= 2) return true;
     }
     if(!nGoodTaus) return false;
-    std::cout<<"..........Dopo"<<std::endl;
 
     for(const pat::Electron& electron : electrons) {
-        if(electron.pt() > 20.0 && std::abs(electron.eta()) < 2.5) {
-            std::cout<<" Ele Missing Hits =  "<<
-                       electron.gsfTrack()->hitPattern().numberOfHits(reco::HitPattern::MISSING_INNER_HITS)<<std::endl;
-            return true;}
+        if(electron.pt() > 15.0 && std::abs(electron.eta()) < 2.5) return true;
     }
 
     for(const pat::Muon& muon : muons) {
-        bool muonIP = fabs(muon.muonBestTrack()->dxy(vtx.position())) < 0.045 &&
-                      fabs(muon.muonBestTrack()->dz(vtx.position())) < 0.2;
-        if(muon.pt() > 17.0 && std::abs(muon.eta())<2.4 && muon.isLooseMuon() && muonIP) {
-            float iso = (muon.pfIsolationR03().sumChargedHadronPt + std::max(
-                       muon.pfIsolationR03().sumNeutralHadronEt +
-                       muon.pfIsolationR03().sumPhotonEt -
-                       0.5 * muon.pfIsolationR03().sumPUPt, 0.0)) / muon.pt();
-
-            std::cout<<" Muon Isolament =  "<<iso<<std::endl;
-
-            return true;
-        }
+        if(muon.pt() > 15.0 && std::abs(muon.eta())<2.4 && muon.isGlobalMuon()) return true;
     }
 
     return false;
